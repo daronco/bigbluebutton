42e8c56eb6fab1a405fcc3f521085b2d4ed49d9a
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-06-17 15:09:12 -0300

    More logs when running rap on docker

--- record-and-playback/core/resque-docker/resque-worker-wrapper.sh
+++ record-and-playback/core/resque-docker/resque-worker-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i "s/redis_host:.*/redis_host: ${REDIS_HOST:-127.0.0.1}/g" ../scripts/bigbluebutton.yml
 sed -i "s/redis_port:.*/redis_port: ${REDIS_PORT:-6379}/g" ../scripts/bigbluebutton.yml
-exec "$@"
+exec "$@" >> /var/log/bigbluebutton/bbb-rap-worker.log 2>&1
